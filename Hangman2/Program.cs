bfc13cdc94457d41471e0284eb024efc82589505
George Sydby <george.sydby@gmail.com>
2021-09-30 09:22:57 +0200

    Add project files.

---
+++ Hangman2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hangman2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void GuessWithAWord()
+        {
+
+        }
+
+        static void GuessWithALetter()
+        {
+
+        }
+    }
+}
